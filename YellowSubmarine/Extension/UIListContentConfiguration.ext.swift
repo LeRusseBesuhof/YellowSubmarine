e23b4f0671d5e75ec1ae5ba501cdc3ab3dab5d9a
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-06-21 00:34:51 +0700

    v 0.15 (load profile data from server)

---
+++ YellowSubmarine/Extension/UIListContentConfiguration.ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIListContentConfiguration.ext.swift
+//  YellowSubmarine
+//
+//  Created by Павел Градов on 20.06.2024.
+//
+
+import Foundation
---
+++ YellowSubmarine/Profile/Model/ProfileTableModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileTableModel.swift
+//  YellowSubmarine
+//
+//  Created by Павел Градов on 20.06.2024.
+//
+
+import Foundation
