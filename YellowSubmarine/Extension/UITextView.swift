d8f2d90836897ca1b119c37999ecbf74ae8c332d
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-07-01 22:16:09 +0700

    v 0.26 (added password reset alertController)

---
+++ YellowSubmarine/Extension/UITextView.swift
@@ -0,0 +1,20 @@
+//
+//  UITextView.swift
+//  YellowSubmarine
+//
+//  Created by Павел Градов on 01.07.2024.
+//
+
+import UIKit
+
+class UITextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
